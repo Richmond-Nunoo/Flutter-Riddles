128a3598e2b0272023dee55c971c8546c3dd942a
Richmond Nunoo <1957richmond@gmail.com>
2023-07-21 14:31:16 +0000

    flashcard cards added. improve code

--- lib/views/flash_cards.dart
+++ lib/views/flash_cards.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:appinio_swiper/appinio_swiper.dart';
+import 'package:flashcards_quiz/views/buttons.dart';
+import 'package:flashcards_quiz/views/example_cards.dart';
+import 'package:flashcards_quiz/views/model.dart';
+import 'package:flutter/cupertino.dart';
 
 class MyCustomWidget extends StatefulWidget {
   const MyCustomWidget({super.key});
@@ -9,94 +12,72 @@ class MyCustomWidget extends StatefulWidget {
 }
 
 class MyCustomWidgetState extends State<MyCustomWidget> {
-  double _page = 10;
-
+  final AppinioSwiperController controller = AppinioSwiperController();
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    PageController pageController;
-    pageController = PageController(initialPage: 10);
-    pageController.addListener(
-      () {
-        setState(
-          () {
-            _page = pageController.page!;
-          },
-        );
-      },
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Flash Cards"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Stack(
-          children: [
-            SizedBox(
-              height: height * 0.7,
-              width: width * .95,
-              child: LayoutBuilder(
-                builder: (context, boxConstraints) {
-                  List<Widget> cards = [];
-
-                  for (int i = 0; i <= 11; i++) {
-                    double currentPageValue = i - _page;
-                    bool pageLocation = currentPageValue > 0;
-
-                    double start = 20 +
-                        max(
-                          (boxConstraints.maxWidth - width * .75) -
-                              ((boxConstraints.maxWidth - width * .75) / 2) *
-                                  -currentPageValue *
-                                  (pageLocation ? 9 : 1),
-                          0.0,
-                        );
-
-                    var customizableCard = Positioned.directional(
-                      top: 20 + 30 * max(-currentPageValue, 0.0),
-                      bottom: 20 + 30 * max(-currentPageValue, 0.0),
-                      start: start,
-                      textDirection: TextDirection.ltr,
-                      child: Container(
-                        height: width * .67,
-                        width: width * .67,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Colors.teal,
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.black.withOpacity(.15),
-                                blurRadius: 10)
-                          ],
-                        ),
-                        child: Text("$i"),
-                      ),
-                    );
-                    cards.add(customizableCard);
-                  }
-                  return Stack(children: cards);
-                },
+    return CupertinoPageScaffold(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.70,
+            child: AppinioSwiper(
+              loop: true,
+              backgroundCardsCount: 2,
+              swipeOptions: const AppinioSwipeOptions.all(),
+              unlimitedUnswipe: true,
+              controller: controller,
+              unswipe: _unswipe,
+              onSwiping: (AppinioSwiperDirection direction) {
+                print("${direction.index}");
+                debugPrint(direction.toString());
+              },
+              onSwipe: _swipe,
+              padding: const EdgeInsets.only(
+                left: 25,
+                right: 25,
+                top: 50,
+                bottom: 40,
               ),
+              onEnd: _onEnd,
+              cardsCount: candidates.length,
+              cardsBuilder: (BuildContext context, int index) {
+                var cardIndex = candidates[index];
+                print(index);
+                return ExampleCard(
+                  candidate: cardIndex,
+                  tap: () {
+                    print(cardIndex.name);
+                  },
+                );
+              },
             ),
-            Positioned.fill(
-              child: PageView.builder(
-                physics: const BouncingScrollPhysics(
-                  parent: AlwaysScrollableScrollPhysics(),
-                ),
-                itemCount: 11,
-                controller: pageController,
-                itemBuilder: (context, index) {
-                  return const SizedBox();
-                },
-              ),
-            ),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          unswipeButton(controller)
+        ],
       ),
     );
   }
+}
+
+void _swipe(int index, AppinioSwiperDirection direction) {
+  print("the card was swiped to the: ${direction.name}");
+  print(index);
+}
+
+void _unswipe(bool unswiped) {
+  if (unswiped) {
+    print("SUCCESS: card was unswiped");
+  } else {
+    print("FAIL: no card left to unswipe");
+  }
+}
+
+void _onEnd() {
+  print("end reached!");
 }
