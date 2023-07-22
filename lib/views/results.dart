0aa086568ebe63e57dd959670b80a21352e4f6df
Richmond Nunoo <1957richmond@gmail.com>
2023-07-22 11:19:52 +0000

    results screen added

---
+++ lib/views/results.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
